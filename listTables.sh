c19fe4aac029ecfcec276656f230af0887d9a2ba
DohaAhmad <103465018+DohaAhmad@users.noreply.github.com>
2025-01-25 23:44:07 +0200

    Add files via upload

--- listTables.sh
+++ listTables.sh
@@ -1,11 +1,13 @@
+
 function listTables(){
     if [[ -z $(ls -p | grep -v /) ]]; then
         echo "No tables were found in this database"
         return 1
-    fi
     
+    else 
     echo "Here are the tables: "
     ls -p | grep -v /
+    fi
 }
 
 export -f listTables
